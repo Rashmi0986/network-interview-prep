c93dc380df6de7e6e87b78b2a8d68c65e00e2b21
Rashmi BS <56731498+Rashmi0986@users.noreply.github.com>
2024-05-24 12:55:57 +0530

    Create GeneratePrefix.py

---
+++ GeneratePrefix.py
@@ -0,0 +1,67 @@
+Given the subnetmask generate the Prefix for the same 
+   - constraints: validate and generate only for valid subnets 
+
+def getSubnetMask(prefix):
+    value = ""
+    if prefix >= 25 and prefix <= 32: #4th octet
+          value = getSubnetValue(prefix)
+          res = "255.255.255."+value
+     
+    elif prefix >= 17 and prefix <= 24: #3rd octet
+          value = getSubnetValue(prefix)
+          res = "255.255."+value+".0"
+         
+    elif prefix >= 9 and prefix <= 16: # 2nd octet
+           value = getSubnetValue(prefix)
+           res = "255."+value+".0.0"
+    else:
+          value = getSubnetValue(prefix)
+          res = value+".0.0.0"
+    return res
+ 
+
+def getSubnetValue(prefix):
+        if prefix == 8 or prefix ==16 or prefix ==24 or prefix == 32:
+            res = 255
+        else:
+            subnetbits = prefix % 8
+            n = 7
+            res = 0
+            while subnetbits:
+                res += 2 ** n
+                n-=1
+                subnetbits-=1
+        return str(res)
+
+#Generating the subnet mask for all the prefix .
+def getallSubnets():
+    subnetMaskSet = set()
+    for i in range(1,33):
+        subnetMaskSet.add(getSubnetMask(i))
+    return subnetMaskSet
+
+def trans(octet):
+    bits = []
+    octet = int(octet)
+    while octet:
+        bits.append(octet%2)
+        octet = octet>>1
+    return bits
+
+# Latest Code with the validation added 
+def getPrefix(subnetMask):
+    allSubnets = getallSubnets()
+    res = ""
+    total = 0
+    if subnetMask in allSubnets:
+        values = subnetMask.split(".")
+        for val in values:
+            res = trans(val)
+            total += res.count(1)
+        return total
+    else:
+        return f"invalid Subnetmask!! can't convert"
+
+s= "128.0.0.0" # valid case
+#s = "255.255.255.256" invalid case
+print(f"{getPrefix(s)}")
